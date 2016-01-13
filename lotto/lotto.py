e6c04293b6cbd0b2dc946392ad72f64cdb9f54fa
palindrome11 <cwconlon@gmail.com>
2016-01-12 16:58:06 -0800

    calc routines

--- lotto/lotto.py
+++ lotto/lotto.py
@@ -22,12 +22,44 @@ class Ticket:
     
     def add_number(self, tnumber):
         self.numbers.append(tnumber)
-    
+            
     def display_numbers(self):
         for row in self.numbers:
             print(row)
 
-
+    def calc_matches(self, pb_number):
+        
+#        number_element=1
+        pb_drawn = pb_number[5]
+        pb_number.pop(5)
+        line=0
+        entry=0
+        for row in self.numbers:
+            line=line+1
+            pb_match=0
+            row = [int(i) for i in row]
+            pb_ticky = row[5]
+            row.pop(5)
+#            print pb_ticky
+#            print pb_drawn
+            if pb_ticky == pb_drawn:
+                pb_match=1
+#                print "Powerball Number Match ", pb_match
+            entry=0
+            for i in row:
+                entry=entry+1
+                for j in pb_number:
+                    if i == j:
+                        print "Number Match ", line, " ", entry, " (", j, ")" , "PB =", pb_match
+                        
+            
+#            row  = [int(i) for i in row]
+#            for x in row:
+#                print x
+#                for y in pb_number:
+#                    if x == y:
+#                        print "Match"
+#            
 
 tick1 = Ticket(123456, "01/8/2016")
 
@@ -38,6 +70,11 @@ with open('../tickets/2016-01-09.csv', 'rU') as csvfile:
 
 
 
-tick1.display_ticket()
-print "Total Tickets: ", Ticket.ticket_total
-tick1.display_numbers()
+#tick1.display_ticket()
+#print "Total Tickets: ", Ticket.ticket_total
+#tick1.display_numbers()
+
+pb=[20,1,3,4,5,19]
+tick1.calc_matches(pb)
+
+
